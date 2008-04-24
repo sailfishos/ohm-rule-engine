2166aac269c67802cdbc1dbe4dbb10a5beec3d62
Krisztian Litkey <kli@kliptop.localdomain>
2008-04-24 11:17:35 +0300

    prolog and factmap test code updates.

--- test/facttest.c
+++ test/facttest.c
@@ -11,81 +11,99 @@
 #define DEVICE        "device"
 #define STATE         "state"
 
-#define fatal(ec, fmt, args...) do {	     \
-    printf("fatal error: "fmt"\n", ## args); \
-    exit(ec);				     \
-  } while (0)
+#define fatal(ec, fmt, args...) do {                    \
+        printf("fatal error: "fmt"\n", ## args);        \
+        exit(ec);                                       \
+    } while (0)
 
 
 
 int
 main(int argc, char *argv[])
 {
-  char *accessories[] = { "ihf", "earpiece", "microphone", NULL }, **acc;
+    char *accessories[] = { "ihf", "earpiece", "microphone", NULL }, **acc;
 
-  OhmFactStore     *store;
-  OhmFact          *fact;
-  OhmFactStoreView *view;
-  OhmPatternMatch  *pm;
-  GValue            dev, state, *d;
-  GSList           *matches, *l;
+    OhmFactStore     *store;
+    OhmFact          *fact;
+    OhmFactStoreView *view;
+    OhmPatternMatch  *pm;
+    OhmPattern       *pattern;
+    GValue            dev, state, *d;
+    GSList           *matches, *l;
 
-  factmap_t        *map;
-  char             *fields[] = { "device", "state", NULL };
+    factmap_t        *map;
+    char             *fields[] = { "device", "state", NULL };
 
-  g_type_init();
+    g_type_init();
 
-  if ((store = ohm_fact_store_new()) == NULL)
-    fatal(1, "failed to create fact store");
+    if ((store = ohm_fact_store_new()) == NULL)
+        fatal(1, "failed to create fact store");
 
-  if ((view = ohm_fact_store_new_view(store, NULL)) == NULL)
-    fatal(2, "failed to create view");
+    if ((view = ohm_fact_store_new_view(store, NULL)) == NULL)
+        fatal(2, "failed to create view");
   
-  if ((map = factmap_create(store, ACCESSORIES, ACCESSORY_KEY, fields)) == NULL)
-    fatal(3, "failed to create factmap for %s", ACCESSORIES);
+    if ((map = factmap_create(store, ACCESSORIES, ACCESSORY_KEY, fields)) == NULL)
+        fatal(3, "failed to create factmap for %s", ACCESSORIES);
   
-  factmap_dump(map);
-
-  l = g_slist_prepend(NULL, ohm_pattern_new(ACCESSORY_KEY));
-  ohm_fact_store_view_set_interested(view, l);
-  
-  for (acc = accessories; *acc; acc++) {
-    fact  = ohm_fact_new(ACCESSORY_KEY);
-    dev   = ohm_value_from_string(*acc);
-    state = ohm_value_from_int(1);
-    ohm_fact_set(fact, DEVICE, &dev);
-    ohm_fact_set(fact, STATE, &state);
-    if (!ohm_fact_store_insert(store, fact))
-      fatal(2, "failed to add device %s to fact store", *acc);
-
-    factmap_update(map);
     factmap_dump(map);
-  }
+
+    pattern = ohm_pattern_new(ACCESSORY_KEY);
+
+#if 0
+    ohm_fact_store_view_add(map->view, OHM_STRUCTURE(pattern));
+#else
+    l = g_slist_prepend(NULL, pattern);
+    ohm_fact_store_view_set_interested(view, l);
+#endif  
+
+    for (acc = accessories; *acc; acc++) {
+        fact  = ohm_fact_new(ACCESSORY_KEY);
+        dev   = ohm_value_from_string(*acc);
+        state = ohm_value_from_int(1);
+        ohm_fact_set(fact, DEVICE, &dev);
+        ohm_fact_set(fact, STATE, &state);
+        if (!ohm_fact_store_insert(store, fact))
+            fatal(2, "failed to add device %s to fact store", *acc);
+
+        factmap_update(map);
+        factmap_dump(map);
+    }
   
-  for (l = ohm_fact_store_get_facts_by_name(store, ACCESSORY_KEY);
-       l != NULL;
-       l = g_slist_next(l)) {
-    fact = (OhmFact *)l->data;
-    d    = ohm_fact_get(fact, DEVICE);
+    for (l = ohm_fact_store_get_facts_by_name(store, ACCESSORY_KEY);
+         l != NULL;
+         l = g_slist_next(l)) {
+        fact = (OhmFact *)l->data;
+        d    = ohm_fact_get(fact, DEVICE);
     
-    if (d == NULL)
-      fatal(3, "failed to retrieve field %s for fact %p", DEVICE, fact);
+        if (d == NULL)
+            fatal(3, "failed to retrieve field %s for fact %p", DEVICE, fact);
 
-    printf("got accessory %s\n", g_value_get_string(d));
-  }
+        printf("got accessory %s\n", g_value_get_string(d));
+    }
 
-  matches = ohm_fact_store_change_set_get_matches(view->change_set);
-  for (l = matches; l != NULL; l = g_slist_next(l)) {
-    if (!OHM_PATTERN_IS_MATCH(l->data))
-      fatal(4, "unexpected object type in changeset");
-    pm = OHM_PATTERN_MATCH(l->data);
-    printf("%s", ohm_pattern_match_to_string(pm));
-  }
+    matches = ohm_fact_store_change_set_get_matches(view->change_set);
+    for (l = matches; l != NULL; l = g_slist_next(l)) {
+        if (!OHM_PATTERN_IS_MATCH(l->data))
+            fatal(4, "unexpected object type in changeset");
+        pm = OHM_PATTERN_MATCH(l->data);
+        printf("%s\n", ohm_pattern_match_to_string(pm));
+    }
 
-  if (matches == NULL) {
-    printf("view had empty changeset\n");
-  }
+    if (matches == NULL) {
+        printf("view had empty changeset\n");
+    }
 
 
-  return 0;
+    return 0;
 }
+
+
+
+
+/* 
+ * Local Variables:
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+*/
+
--- test/prologtest.c
+++ test/prologtest.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <prolog/prolog.h>
 
@@ -14,25 +15,61 @@
 int
 main(int argc, char *argv[])
 {
-    prolog_export_t *exported, *e;
+    char *default_files[] = {
+        "prolog/hwconfig",
+        "prolog/devconfig",
+        "prolog/interface",
+        "prolog/profile",
+        "prolog/audio",
+        "prolog/test"
+    };
+    char               **files;
+    int                  nfile = sizeof(default_files)/sizeof(default_files[0]);
+
+    prolog_predicate_t   *predicates, *p, *set_routes;
+    char               ***actions;
+    int                   i;
 
     /* initialize our prolog library */
     if (prolog_init("test", 0, 0, 0, 0) != 0)
         fatal(1, "failed to initialize prolog library");
 
-    /* load our test file */
-    if (prolog_load_file(argc < 2 ? TESTPL : argv[1]) != 0)
-        fatal(2, "failed to load %s", TESTPL);
+    if (argc <= 1)
+        files = default_files;
+    else {
+        files = argv + 1;
+        nfile = argc - 1;
+    }
+        
+    /* load our test files */
+    for (i = 0; i < nfile; i++) {
+        printf("loading file %s...\n", files[i]);
+        if (prolog_load_file(files[i]))
+            fatal(2, "failed to load %s", files[i]);
+    }
     
-    if ((exported = prolog_get_exported(NULL)) == NULL)
-        fatal(3, "failed to get exports from prolog");
+    if ((predicates = prolog_predicates(NULL)) == NULL)
+        fatal(3, "failed to get exported predicates from prolog");
 
-    for (e = exported; e->name; e++)
+    set_routes = NULL;
+    for (p = predicates; p->name; p++) {
         printf("found exported predicate: %s%s%s/%d (0x%x)\n",
-               e->module ?: "", e->module ? ":" : "",
-               e->name, e->arity,
-               (unsigned int)e->predicate);
+               p->module ?: "", p->module ? ":" : "",
+               p->name, p->arity,
+               (unsigned int)p->predicate);
+        if (!strcmp(p->name, "set_routes") && p->arity == 1)
+            set_routes = p;
+    }
+
+    if (set_routes == NULL)
+        fatal(4, "failed to find exported predicate \"set_routes\"");
     
+    printf("invoking prolog predicate %s\n", set_routes->name);
+    if (!prolog_call(set_routes, &actions))
+        fatal(5, "failed to invoke exported predicate %s", set_routes->name);
+    
+    prolog_dump_actions(actions);
+
     /* clean up our prolog library */
     prolog_exit();
     
